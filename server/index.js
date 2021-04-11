5562384ba55e1de6918481c709a5e47ca91652c0
Shawn <shawn.harris3510@gmail.com>
2021-04-11 11:59:11 -0400

    postActivity Schema

--- server/index.js
+++ server/index.js
@@ -4,23 +4,27 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 
+import postRoutes from "./routes/posts.js"
+
 const app = express();
 
 // PARSE REGISTERED MIDDLEWARE
-app.use(express.urlencoded({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 
+// GET ALL ROUTES
+app.use("/posts", postRoutes)
 
 // PARSE HTTP REQUEST FROM JSON STRING
-app.use(express.json({limit: "30mb", extended: true}));
+app.use(bodyParser.json({limit: "30mb", extended: true}));
 
 // PARSE CORS REQUESTS
 app.use(cors());
 
 // GRAB DB KEYS
 dotenv.config()
-// verify dotenv working
 // console.log(process.env)
 const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.hbh5c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
 // PORT NUMBER
 const PORT = process.env.PORT || 5000
 
