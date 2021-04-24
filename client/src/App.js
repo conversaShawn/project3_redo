c743429665707681e44b764e028e948dd34b0f24
Shawn <shawn.harris3510@gmail.com>
2021-04-24 15:31:25 -0400

    correcting styles.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,17 @@ import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
 import Activities from "./components/Activities/Activities"
 import Form from "./components/Form/Form"
-
-import memories from "./images/giphy.gif"
+import memories from "./images/memories.png"
+import useStyles from "./styles"
 
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Memories</Typography>
-        <img src={memories} alt="Memories" height="60"/>
+    <Container maxidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="Memories" height="60"/>
       </AppBar>
       <Grow in>
         <Container>
