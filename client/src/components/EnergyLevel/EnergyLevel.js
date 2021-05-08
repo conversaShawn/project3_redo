bfbaea5721585337e1febe304f1c91a8e5a8da6e
Shawn <shawn.harris3510@gmail.com>
2021-05-07 21:55:49 -0400

    adding energyLevels

--- client/src/components/EnergyLevel/EnergyLevel.js
+++ client/src/components/EnergyLevel/EnergyLevel.js
@@ -1,11 +1,13 @@
 import react from 'react';
+import { FaStar } from 'react-icons/fa';
 
 export default (EnergyLevel = () => {
 	return (
 		<div>
-			{[ ...Array(5) ].map((battery) => {
+			{[ ...Array(5) ].map((star, i) => {
 				return <label>
-                    battery
+                    <input type='radio' name='rating'></input>
+					<FaStar className='star' size={100} />
                 </label>
 			})}
 		</div>
