1760b339adcaff8aaa1b348c2854a4fdd2b75bfe
Shawn <shawn.harris3510@gmail.com>
2021-05-16 22:11:42 -0400

    energy level is still broken

---
+++ client/src/components/EnergyLevel/EnergyLevel.js
@@ -0,0 +1,28 @@
+import { React, useState } from 'react';
+// import Rating from '@material-ui/lab/Rating'
+import StarIcon from '@material-ui/icons/Star'
+
+import useStyles from './styles';
+
+const EnergyLevel = (e) => {
+	// const [rating, setRating] = useState(null)
+	const classes = useStyles();
+	const [rating, setRating] = useState(null);
+
+	return (
+		<div>
+			{[ ...Array(5)].map((star, i) => {
+				const ratingValue = i + 1;
+				
+				return(
+					<label key={i} >
+						<input className={classes.starInput}  type='radio' name='rating' value={ratingValue} onClick={() => setRating(ratingValue)}/>
+						<StarIcon className={classes.stars} size='large' color={ratingValue <= rating ? 'primary' : 'secondary'}/>
+					</label>
+				)
+			})}
+		</div>
+	)
+}
+
+export default EnergyLevel;
